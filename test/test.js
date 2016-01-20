539228a816d0571e347c05cd22799461ef4c0766
Oskar Segersvärd <victorystick@gmail.com>
2016-01-20 10:39:52 +0100

    Add failing test for 'export class ...'

--- test/test.js
+++ test/test.js
@@ -22,6 +22,23 @@ describe( 'rollup-plugin-typescript', function () {
 		});
 	});
 
+	it( 'transpiles `export class A` correctly', function () {
+		return rollup.rollup({
+			entry: 'sample/export-class-fix/main.ts',
+			plugins: [
+				typescript()
+			]
+		}).then( function ( bundle ) {
+			const generated = bundle.generate();
+			const code = generated.code;
+
+			assert.equal( code.indexOf( 'class' ), -1, code );
+			assert.ok( code.indexOf( 'var A = (function' ) !== -1, code );
+			assert.ok( code.indexOf( 'var B = (function' ) !== -1, code );
+			assert.ok( code.indexOf( 'export { A, B };' ) !== -1, code );
+		});
+	});
+
 	it( 'transpiles ES6 features to ES5 with source maps', function () {
 		return rollup.rollup({
 			entry: 'sample/import-class/main.ts',
@@ -32,9 +49,9 @@ describe( 'rollup-plugin-typescript', function () {
 			const generated = bundle.generate();
 			const code = generated.code;
 
-			assert.ok( code.indexOf( 'class' ) === -1, code );
-			assert.ok( code.indexOf( '...' ) === -1, code );
-			assert.ok( code.indexOf( '=>' ) === -1, code );
+			assert.equal( code.indexOf( 'class' ), -1, code );
+			assert.equal( code.indexOf( '...' ), -1, code );
+			assert.equal( code.indexOf( '=>' ), -1, code );
 		});
 	});
 
