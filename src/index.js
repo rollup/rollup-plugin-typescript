f233e0df4ed397c7ad0f216c134b4e1cd73f76fa
Bogdan Chadkin <trysound@yandex.ru>
2016-08-05 16:57:23 +0300

    Split index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import * as ts from 'typescript';
 import { createFilter } from 'rollup-pluginutils';
 import * as path from 'path';
-import {
-	existsSync,
-	readFileSync,
-	statSync,
-} from 'fs';
 import assign from 'object-assign';
 import compareVersions from 'compare-versions';
 
 import { endsWith } from './string';
+import { getDefaultOptions, compilerOptionsFromTsConfig, adjustCompilerOptions } from './options.js';
 import fixExportClass from './fixExportClass';
+import resolveHost from './resolveHost';
+
 /*
 interface Options {
 	tsconfig?: boolean;
@@ -20,89 +18,6 @@ interface Options {
 	module?: string;
 }
 */
-const resolveHost = {
-	directoryExists ( dirPath ) {
-		try {
-			return statSync( dirPath ).isDirectory();
-		} catch ( err ) {
-			return false;
-		}
-	},
-	fileExists ( filePath ) {
-		try {
-			return statSync( filePath ).isFile();
-		} catch ( err ) {
-			return false;
-		}
-	}
-};
-
-function goodErrors ( diagnostic ) {
-	// All errors except `Cannot compile modules into 'es6' when targeting 'ES5' or lower.`
-	return diagnostic.code !== 1204;
-}
-
-function getDefaultOptions() {
-	return {
-		noEmitHelpers: true,
-		module: 'es2015',
-		sourceMap: true
-	};
-}
-
-// Gratefully lifted from 'look-up', due to problems using it directly:
-//   https://github.com/jonschlinkert/look-up/blob/master/index.js
-//   MIT Licenced
-function findFile( cwd, filename ) {
-	let fp = cwd ? ( cwd + '/' + filename ) : filename;
-
-	if ( existsSync( fp ) ) {
-		return fp;
-	}
-
-	const segs = cwd.split( path.sep );
-	let len = segs.length;
-
-	while ( len-- ) {
-		cwd = segs.slice( 0, len ).join( '/' );
-		fp = cwd + '/' + filename;
-		if ( existsSync( fp ) ) {
-			return fp;
-		}
-	}
-
-	return null;
-}
-
-function compilerOptionsFromTsConfig( typescript ) {
-	const cwd = process.cwd();
-
-	const tsconfig = typescript.readConfigFile( findFile( cwd, 'tsconfig.json' ), path => readFileSync( path, 'utf8' ) );
-
-	if ( !tsconfig.config || !tsconfig.config.compilerOptions ) return {};
-
-	return tsconfig.config.compilerOptions;
-}
-
-function adjustCompilerOptions( typescript, options ) {
-	// Set `sourceMap` to `inlineSourceMap` if it's a boolean
-	// under the assumption that both are never specified simultaneously.
-	if ( typeof options.inlineSourceMap === 'boolean' ) {
-		options.sourceMap = options.inlineSourceMap;
-		delete options.inlineSourceMap;
-	}
-
-	// Delete the `declaration` option to prevent compilation error.
-	// See: https://github.com/rollup/rollup-plugin-typescript/issues/45
-	delete options.declaration;
-
-	const tsVersion = typescript.version.split('-')[0];
-	if ( 'strictNullChecks' in options && compareVersions( tsVersion, '1.9.0' ) < 0 ) {
-		delete options.strictNullChecks;
-
-		console.warn( `rollup-plugin-typescript: 'strictNullChecks' is not supported; disabling it` );
-	}
-}
 
 export default function typescript ( options ) {
 	options = assign( {}, options || {} );
@@ -188,8 +103,9 @@ export default function typescript ( options ) {
 				compilerOptions
 			});
 
+			// All errors except `Cannot compile modules into 'es6' when targeting 'ES5' or lower.`
 			const diagnostics = transformed.diagnostics ?
-				transformed.diagnostics.filter( goodErrors ) : [];
+				transformed.diagnostics.filter( diagnostic => diagnostic.code !== 1204 ) : [];
 
 			let fatalError = false;
 
---
+++ src/options.js
@@ -0,0 +1,68 @@
+import * as path from 'path';
+import compareVersions from 'compare-versions';
+import {
+	existsSync,
+	readFileSync
+} from 'fs';
+
+export function getDefaultOptions() {
+	return {
+		noEmitHelpers: true,
+		module: 'es2015',
+		sourceMap: true
+	};
+}
+
+// Gratefully lifted from 'look-up', due to problems using it directly:
+//   https://github.com/jonschlinkert/look-up/blob/master/index.js
+//   MIT Licenced
+function findFile( cwd, filename ) {
+	let fp = cwd ? ( cwd + '/' + filename ) : filename;
+
+	if ( existsSync( fp ) ) {
+		return fp;
+	}
+
+	const segs = cwd.split( path.sep );
+	let len = segs.length;
+
+	while ( len-- ) {
+		cwd = segs.slice( 0, len ).join( '/' );
+		fp = cwd + '/' + filename;
+		if ( existsSync( fp ) ) {
+			return fp;
+		}
+	}
+
+	return null;
+}
+
+export function compilerOptionsFromTsConfig( typescript ) {
+	const cwd = process.cwd();
+
+	const tsconfig = typescript.readConfigFile( findFile( cwd, 'tsconfig.json' ), path => readFileSync( path, 'utf8' ) );
+
+	if ( !tsconfig.config || !tsconfig.config.compilerOptions ) return {};
+
+	return tsconfig.config.compilerOptions;
+}
+
+export function adjustCompilerOptions( typescript, options ) {
+	// Set `sourceMap` to `inlineSourceMap` if it's a boolean
+	// under the assumption that both are never specified simultaneously.
+	if ( typeof options.inlineSourceMap === 'boolean' ) {
+		options.sourceMap = options.inlineSourceMap;
+		delete options.inlineSourceMap;
+	}
+
+	// Delete the `declaration` option to prevent compilation error.
+	// See: https://github.com/rollup/rollup-plugin-typescript/issues/45
+	delete options.declaration;
+
+	const tsVersion = typescript.version.split('-')[0];
+	if ( 'strictNullChecks' in options && compareVersions( tsVersion, '1.9.0' ) < 0 ) {
+		delete options.strictNullChecks;
+
+		console.warn( `rollup-plugin-typescript: 'strictNullChecks' is not supported; disabling it` );
+	}
+}
